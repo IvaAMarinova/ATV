79df3c9079ecbee76852d4b1d3fd2df2a3ecf7e8
Iva Marinova <ivamarinova06@gmail.com>
2024-01-21 14:14:59 +0200

    Implemented get wish functionality

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from app.extensions import db
 from app.model.wish import Wish
-from app.controller.wish_controller import add_wish, remove_wish
+from app.controller.wish_controller import add_wish, remove_wish, get_wish
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:iva123@localhost:3307/wishes'
@@ -11,19 +11,19 @@ db.init_app(app)
 
 app.route('/add_wish', methods=['POST'])(add_wish)
 app.route('/wishes/<int:wish_id>', methods=['DELETE'])(remove_wish)
+app.route('/wishes/<int:wish_id>', methods=['GET'])(get_wish)
 
 if __name__ == '__main__':
     with app.app_context():
-        test_wish = Wish(content='Test Wish')
-        db.session.add(test_wish)
+        test_wish1 = Wish(content='Test Wish aaaaaaaaa')
+        test_wish2 = Wish(content='Test Wish bbbbbbbbbb')
+        db.session.add_all([test_wish1, test_wish2])
         db.session.commit()
 
-        remove_wish(test_wish.id)
-
-        deleted_wish = Wish.query.get(test_wish.id)
-        if deleted_wish is None:
-            print(f'Wish with ID {test_wish.id} successfully removed.')
-        else:
-            print(f'Wish with ID {test_wish.id} was not removed.')
+        wish_id_to_get = test_wish1.id
+        print(f'\nGetting Wish with ID: {wish_id_to_get}')
+        response, _ = get_wish(wish_id_to_get)
+        wish_data = response.get_json()
+        print(f'Wish ID: {wish_data["id"]}, Content: "{wish_data["content"]}", Timestamp: {wish_data["timestamp"]}, Likes: {wish_data["likes"]}')
 
     app.run(debug=True)
--- app/controller/wish_controller.py
+++ app/controller/wish_controller.py
@@ -21,4 +21,12 @@ def remove_wish(wish_id):
         return jsonify({'message': 'Wish removed successfully'}), 200
     else:
         return jsonify({'message': 'Wish not found'}), 404
+    
+def get_wish(wish_id):
+    wish = Wish.query.get(wish_id)
+    if wish:
+        return jsonify({'id': wish.id, 'content': wish.content, 'timestamp': wish.timestamp, 'likes': wish.likes}), 200
+    else:
+        return jsonify({'message': 'Wish not found'}), 404
+
 
